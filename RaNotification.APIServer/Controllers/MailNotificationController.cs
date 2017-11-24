7dd0f4c1fa30af52d44511ade4431c329f3a59b4
HHDong1988 <donghuanhuan1988@hotmail.com>
2017-11-24 13:50:28 +0800

    MailNotification

---
+++ RaNotification.APIServer/Controllers/MailNotificationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RaNotification.Data;
+using RaNotification.Data.Mail;
+using RaNotification.Way.Mail;
+
+namespace RaNotification.APIServer.Controllers
+{
+    public class MailNotificationController : ApiController
+    {
+        // POST: api/MailNotification
+        public IHttpActionResult Post([FromBody]MailEntity entity)
+        {
+            try
+            {
+                var agent = new MailAgent(
+                    new MailConfig
+                    {
+                        SmtpServer = "mailrelay.ra.rockwell.com",
+                        Port = 25
+                    });
+
+                if(!agent.Send(entity))
+                {
+                    return BadRequest();
+                }
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
